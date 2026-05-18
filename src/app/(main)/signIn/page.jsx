fe3bcfe2255803eac70f09b581aaf1c2b4751293
diponkorroy064-max <diponkorroy064@gmail.com>
2026-05-18 20:34:02 +0600

    add functionality in  signin and signup page

--- src/app/(main)/signIn/page.jsx
+++ src/app/(main)/signIn/page.jsx
@@ -1,12 +1,38 @@
+'use client'
 import { Check } from '@gravity-ui/icons';
 import { Button, FieldError, Form, Input, Label, TextField } from '@heroui/react';
 import Link from 'next/link';
-import React from 'react';
+import React, { useState } from 'react';
+import { FaEye, FaEyeSlash } from 'react-icons/fa';
+import { FcGoogle } from 'react-icons/fc';
+
 
 const SignInPage = () => {
-    const onSubmit = () => {
-        
+    const [isShowPass, setIsShowPass] = useState(false);
+    
+    
+    const onSubmit = async (e) => {
+        e.preventDefault();
+        const formData = new FormData(e.currentTarget);
+        const user = Object.fromEntries(formData.entries());
+        console.log(user);
+
+        // const { data, error } = await authClient.signIn.email({
+        //     email: user.email,
+        //     password: user.password,
+        //     rememberMe: true,
+        //     callbackURL: "/",
+        // });
+        // console.log("sign in response", data, error);
     }
+
+    // const handleSigninGoogle = async () => {
+    //     await authClient.signIn.social({
+    //         provider: "google",
+    //     });
+    // };
+
+
 
     return (
         <div>
@@ -16,33 +42,50 @@ const SignInPage = () => {
                     <p> Resume your adventure with </p>
                 </div>
 
-                <div className='mx-auto border border-gray-400 shadow w-100 p-10 space-y-3'>
-                    <Form className="flex flex-col gap-4 space-y-3">
+                <div className='mx-auto border border-gray-400 shadow w-100 p-10 space-y-3 rounded-2xl'>
+                    <Form onSubmit={onSubmit} className="flex flex-col gap-4 space-y-3">
 
                         <TextField
                             isRequired
                             name="email"
                             type="email"
-                            >
+                            validate={(value) => {
+                                if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(value)) {
+                                    return "Please enter a valid email address";
+                                }
+                                return null;}}>
                             <Label>Email</Label>
                             <Input placeholder="Enter Your Email" />
                             <FieldError />
                         </TextField>
 
                         <TextField
-                            isRequired
+                            isRequired 
+                            className="relative"
                             minLength={8}
                             name="password"
                             type="password"
-                            >
+                            validate={(value) => {
+                                if (value.length < 8) {
+                                    return "Password must be at least 8 characters";
+                                }
+                                if (!/[A-Z]/.test(value)) {
+                                    return "Password must contain at least one uppercase letter";
+                                }
+                                if (!/[0-9]/.test(value)) {
+                                    return "Password must contain at least one number";
+                                }
+                                return null;}}>
                             <Label>Password</Label>
                             <Input placeholder="Enter your password" />
                             <FieldError />
-                            <div className='flex justify-between items-center text-[12px] mt-3'>
-                                <h2>Remember Me</h2>
-                                <h2>Forget Password ?</h2>
-                            </div>
+                            <p className='absolute top-8 right-3 text-xl' onClick={() => setIsShowPass(!isShowPass)}>{isShowPass ? <FaEyeSlash/> : <FaEye />}</p>
                         </TextField>
+
+                        <div className='flex justify-between items-center text-[12px]'>
+                            <h2>Remember Me</h2>
+                            <h2>Forget Password ?</h2>
+                        </div>
 
                         <div className="flex gap-2">
                             <Button className="w-full rounded-none" type="submit">
@@ -57,7 +100,7 @@ const SignInPage = () => {
                     </Form>
 
                     <div className='flex justify-center flex-col space-y-3'>
-                        <Button className="w-full rounded-none">Continue with Google</Button>
+                        <Button className="w-full rounded-none border border-gray-400 text-gray-900 bg-white"><FcGoogle /> Continue with Google</Button>
 
                         <h2 className='text-center'>Don`t have an account? <span><Link className='text-red-500' href={'/signUp'}>Sign Up</Link></span></h2>
                     </div>
@@ -68,4 +111,5 @@ const SignInPage = () => {
 };
 
 export default SignInPage;
+
 
--- src/app/(main)/signUp/page.jsx
+++ src/app/(main)/signUp/page.jsx
@@ -1,20 +1,58 @@
+'use client'
 import { Check } from '@gravity-ui/icons';
 import { Button, FieldError, Form, Input, Label, TextField } from '@heroui/react';
 import Link from 'next/link';
-import React from 'react';
+import React, { useState } from 'react';
+import { FaEye } from 'react-icons/fa';
+import { FaEyeSlash } from 'react-icons/fa6';
+import { FcGoogle } from 'react-icons/fc';
 
 
 
 const SignUpPage = () => {
+    const [isShowPass, setIsShowPass] = useState(false);
+
+    const onSubmit = async (e) => {
+        e.preventDefault();
+        const formData = new FormData(e.currentTarget);
+        const user = Object.fromEntries(formData.entries());
+        console.log(user);
+
+        // const { data, error } = await authClient.signUp.email({
+        //     name: user.name,
+        //     image: user.image,
+        //     email: user.email,
+        //     password: user.password,
+        //     callbackURL: "/signIn",
+        // });
+
+        // console.log("sign up response", data, error);
+
+        // if (error) {
+        //     toast.error("Sign up failed " + error.message);
+        // }
+        // else if (data) {
+        //     toast.success("Sign up successfull! Verify your Email")
+        // }
+    }
+
+    // const handleSigninGoogle = async () => {
+    //     await authClient.signIn.social({
+    //         provider: "google",
+    //         callbackURL: "signIn"
+    //     })
+    // }
+
+
     return (
         <div className='container mx-auto py-20 space-y-5'>
             <div className='text-center'>
                 <h2 className='text-3xl font-extrabold'>Create Account</h2>
-                <p>Start your adventure with Wanderlust</p>
+                <p>Start your advent</p>
             </div>
 
-            <div className='mx-auto border border-gray-400 shadow w-150 p-10 space-y-3'>
-                <Form  className="flex flex-col gap-4 space-y-3" >
+            <div className='mx-auto border border-gray-400 rounded-2xl shadow w-150 p-10 space-y-3'>
+                <Form onSubmit={onSubmit} className="flex flex-col gap-4 space-y-3" >
 
                     <TextField
                         isRequired
@@ -38,7 +76,11 @@ const SignUpPage = () => {
                         isRequired
                         name="email"
                         type="email"
-                        >
+                        validate={(value) => {
+                            if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(value)) {
+                                return "Please enter a valid email address";
+                            }
+                            return null;}}>
                         <Label>Email</Label>
                         <Input placeholder="Enter Your Email" />
                         <FieldError />
@@ -46,30 +88,54 @@ const SignUpPage = () => {
 
                     <TextField
                         isRequired
-                        minLength={8}
+                        minLength={8} 
+                        className="relative"
                         name="password"
-                        type="password"
-                        >
+                        type={isShowPass?"text":"password"}
+                        validate={(value) => {
+                            if (value.length < 8) {
+                                return "Password must be at least 8 characters";
+                            }
+                            if (!/[A-Z]/.test(value)) {
+                                return "Password must contain at least one uppercase letter";
+                            }
+                            if (!/[0-9]/.test(value)) {
+                                return "Password must contain at least one number";
+                            }
+                            return null;}}>
                         <Label>Password</Label>
                         <Input placeholder="Create a password" />
                         <FieldError />
+                        <p className='absolute top-8 right-3 text-xl' onClick={()=>setIsShowPass(!isShowPass)}>{isShowPass? <FaEyeSlash/>:<FaEye/>}</p>
                     </TextField>
 
                     <TextField
                         isRequired
-                        minLength={8}
+                        minLength={8} 
+                        className="relative"
                         name="password"
-                        type="password"
-                        >
+                        type={isShowPass ? "text" : "password"}
+                        validate={(value) => {
+                            if (value.length < 8) {
+                                return "Password must be at least 8 characters";
+                            }
+                            if (!/[A-Z]/.test(value)) {
+                                return "Password must contain at least one uppercase letter";
+                            }
+                            if (!/[0-9]/.test(value)) {
+                                return "Password must contain at least one number";
+                            }
+                            return null;}}>
                         <Label>Confirm Password</Label>
                         <Input placeholder="Confirm your password" />
                         <FieldError />
-
-                        <div className='flex justify-between items-center text-[12px] mt-3'>
-                            <h2>Remember Me</h2>
-                            <h2>Forget Password ?</h2>
-                        </div>
+                        <p className='absolute top-8 right-3 text-xl' onClick={() => setIsShowPass(!isShowPass)}>{isShowPass ? <FaEyeSlash /> : <FaEye />}</p>
                     </TextField>
+
+                    <div className='flex justify-between items-center text-[12px]'>
+                        <h2>Remember Me</h2>
+                        <h2>Forget Password ?</h2>
+                    </div>
 
                     <div className="flex gap-2">
                         <Button className="w-full rounded-none" type='submit'>
@@ -84,7 +150,7 @@ const SignUpPage = () => {
                 </Form>
 
                 <div className='flex justify-center flex-col space-y-3'>
-                    <Button className="w-full rounded-none">Continue with Google</Button>
+                    <Button className="w-full rounded-none bg-white border border-gray-300 hover:bg-cyan-200 text-gray-900"><FcGoogle /> Continue with Google</Button>
 
                     <h2 className='text-center'>Already have an account? <span><Link className='text-red-500' href={'/signIn'}>Sign In</Link></span></h2>
                 </div>
